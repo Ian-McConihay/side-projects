01616f75819c5eaba10bcad2e727f722ea93bfb7
Ian McConihay <mcconihay.ian92@gmail.com>
2021-09-01 09:30:28 -0500

    eddabit work

---
+++ edabit.js
@@ -0,0 +1,9 @@
+function triArea(base, height) {
+    return (base * height) / 2
+}
+
+function cubes(a) {
+    return a ** 3
+}
+// 3 ** 4 = 81 so it takes the first number and multiplies itself based om the second number
+//3 * 3 * 3 * 3 = 81
