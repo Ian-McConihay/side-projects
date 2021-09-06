52a52190341ba945f1bffd1d13b620d3466601b1
Ian McConihay <mcconihay.ian92@gmail.com>
2021-09-06 15:14:33 -0500

    edabit

--- edabit.js
+++ edabit.js
@@ -7,6 +7,29 @@ function cubes(a) {
 }
 // 3 ** 4 = 81 so it takes the first number and multiplies itself based om the second number
 //3 * 3 * 3 * 3 = 81
+
+
 function numArgs() {
     return arguments.length
 }
+//This function returns the arguments by number. (x,y,z) return 3
+
+function addition(num) {
+    return num + 1
+}
+//This function takes in a single argument and returns it plus 1
+
+function findPerimeter(length, width) {
+    return (length * 2) + (width * 2)
+}
+// This function returns a rectangle perimeter
+
+function addition(a, b) {
+    return a + b
+}
+//this function returns two functions added together
+
+function sumPolygon(n) {
+    return  (n - 2) * 180
+}
+//This function returns the sum of a polygon
